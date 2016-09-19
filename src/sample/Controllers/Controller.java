4b02d42b1d1e2a11065be1e0a2f73a1f2caed953
Jam Marcos Hernandez Quiceno <jamarck96@gmail.com>
2016-09-19 15:21:33 -0400

    Added ArchitecturalInvariantInterpretation which receives an post-dikon filtered report and allows the user to obtain data as needed. (Fixed)

--- src/sample/Controllers/Controller.java
+++ src/sample/Controllers/Controller.java
@@ -1,10 +1,15 @@
 package sample.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import sample.Logic.DatabaseOperations;
 import sample.Logic.MenuSettings;
+import sample.Logic.TestSuite;
 
 import java.io.IOException;
 import java.net.URL;
@@ -12,12 +17,27 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    private boolean environmentSetup = false;
+
     @FXML private javafx.scene.control.MenuItem settings;
-    @FXML private MenuItem close;
-    @FXML private TreeTableColumn nodesMainTreeTable;
+    @FXML private Line mainEnvironmentNotSetupLine = new Line();
+    @FXML private Text mainEnvironmentNotSetupLabel = new Text();
+    @FXML private Button architecturalInvariantTest = new Button();
 
     @Override
-    public void initialize(URL location, ResourceBundle resources){
+    public void initialize(URL location, ResourceBundle resources) {
+        DatabaseOperations databaseOperations = new DatabaseOperations();
+
+
+        if(!(databaseOperations.retrieveData("extDir").equals("/...") || databaseOperations.retrieveData("extDaikon").equals("/..."))){
+            environmentSetup = true;
+            mainEnvironmentNotSetupLine.setVisible(false);
+            mainEnvironmentNotSetupLabel.setVisible(false);
+            architecturalInvariantTest.setVisible(true);
+        }else {
+            architecturalInvariantTest.setVisible(false);
+        }
+
         assert settings != null : "fx:id=\"settings\" was not injected: check your FXML file 'sample.fxml'.";
         MenuSettings menuSettings = new MenuSettings();
         settings.setOnAction(e ->{
@@ -28,12 +48,17 @@ public class Controller implements Initializable {
             }
             System.out.println("It works");
         });
-        close.setOnAction(e->System.exit(0));
-
 
         /*MenuImplementation.setOnAction(event -> {
             System.out.print("hello");
         });
         */
+    }
+
+    @FXML
+    public void architecturalInvatiantTest(ActionEvent event){
+        TestSuite testSuite = new TestSuite("Architecture");
+        testSuite.display();
+        System.out.print("In");
     }
 }
