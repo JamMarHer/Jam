d59145aa1f1bc7a1abd30b3b314f0705baf2780d
Jam <jamarck96@gmail.com>
2016-09-23 08:57:14 -0400

    Small Debug Reset - INVISIBLE~VISIBLE Stack Pane

---
+++ src/sample/Logic/TestingThread.java
@@ -0,0 +1,14 @@
+package sample.Logic;
+
+import sample.Interfaces.ReportInterpretation;
+import sample.Properties.TestingProgress;
+
+/**
+ * Created by CCC on 9/21/2016.
+ */
+public class TestingThread extends Thread {
+
+    public TestingThread (ReportInterpretation AII, TestingProgress TP, String projectTestPath, String projectTestLaunchRun){
+
+    }
+}
